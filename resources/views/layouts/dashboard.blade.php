3d5d38f0156c88b4282ea32d0baaf6efee90d4f4
Richard Mikhael <richrdpere321@gmail.com>
2022-08-01 06:27:01 -0500

    Proyecto desde la Casa 01/08/2022

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -122,8 +122,8 @@
                                     <td>{{$boleta->oficina}}</td>
                                     <td>{{$boleta->motivo}}</td>
                                     <td class="td-actions text-center;">
-                                        <div class="container">
-                                            <a class="btn btn-warning " data-bs-toggle="modal" data-bs-target="#miModal" href="{{ route('boletas.show', $boleta) }}">
+                                        <div class="container flex: justify-items-center">
+                                            <a class="btn btn-warning hero__cta" data-bs-toggle="modal" data-bs-target="#miModal" href="{{ route('boletas.show', $boleta) }}">
                                                 <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg>
                                             </a>
                                              
@@ -247,11 +247,23 @@
                                             
 
                                             <a class="btn btn-info" href="{{ route('boletas.edit', $boleta) }}"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"/></svg></a>
-                                                <form action="{{ route('boletas.delete', $boleta->id) }}" class="d-inline formulario-eliminar" method="POST" >
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="btn btn-danger" type="submit" rel="tooltip"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg></button>
-                                                </form>
+                                            <form action="{{ route('boletas.delete', $boleta->id) }}" class="d-inline formulario-eliminar" method="POST" >
+                                                @csrf
+                                                @method('DELETE')
+                                                     <button class="btn btn-danger" type="submit" rel="tooltip"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg></button>
+                                            </form>
+
+                                            <fieldset disabled="disabled">
+                                                @if ($boleta->permisos->count())
+                                                    
+                                                    <input type="button" value="Firmado" style="color:Black; display:flex; justify-content: center; background-color: rgb(219, 123, 13); padding: 0.5rem">
+                                                    
+                                                        
+                                                            
+                                                @else
+                                                    <input type="button" value="Sin Firmar" style="color:Black; display:flex; justify-content: center; background-color: rgb(38, 86, 189); padding: 0.5rem">
+                                                @endif     
+                                            </fieldset>                                       
                                         </div>              
                                     </td>
                                 </tr>                       
@@ -293,19 +305,28 @@
                                 </thead>
                 
                                 <tbody>
-                                    @foreach ($user->boletas as $boleta)
+                                    @foreach ($user->boletas as $boleta)                                    
+                                        <tr>
+                                            <td>{{$boleta->id}}</td>
+                                            <td>{{$boleta->nombre}}</td>
+                                            <td>{{$boleta->fecha}}</td>
+                                            <td>{{$boleta->motivo}}</td>
+                                                
+                                            <td>
+                                                <fieldset disabled="disabled">
+                                                    @if ($boleta->permisos->count())
+                                                        
+                                                        <input type="button" value="Firmado" style="color:Black; display:flex; justify-content: center; background-color: rgb(219, 123, 13); padding: 0.5rem">
+     
+                                                                
+                                                    @else
+                                                        <input type="button" value="Sin Firmar" style="color:Black; display:flex; justify-content: center; background-color: rgb(38, 86, 189); padding: 0.5rem">
+                                                    @endif     
+                                                </fieldset>       
+                                                
                                             
-                                        
-                                        @foreach ($boleta->permisos as $permiso)
-                                            <tr>
-                                                <td>{{$boleta->id}}</td>
-                                                <td>{{$boleta->nombre}}</td>
-                                                <td>{{$boleta->fecha}}</td>
-                                                <td>{{$boleta->motivo}}</td>
-                                                    
-                                                <td>{{$permiso->firma}}</td>                                                
-                                            </tr>  
-                                        @endforeach  
+                                            </td>                                                
+                                        </tr>  
                                     @endforeach                      
                                 </tbody>
                     
@@ -331,6 +352,9 @@
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap4.min.js"></script>
+    <!--    SCRIPT  -   MODAL   -->
+    <script src="resources/js/modal.js"></script>
+
     <script>
         $('#boletas').DataTable({
             responsive: true,
